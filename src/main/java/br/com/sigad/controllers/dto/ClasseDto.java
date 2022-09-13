88e41ae12fc5570464677e47854099813541785b
hgrafa <hugorafell@gmail.com>
2022-09-13 15:27:53 -0300

    Creating new Classe DTO

---
+++ src/main/java/br/com/sigad/controllers/dto/ClasseDto.java
@@ -0,0 +1,23 @@
+package br.com.sigad.controllers.dto;
+
+import br.com.sigad.entities.Classe;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class ClasseDto {
+	
+	private Long codigo;
+	private String nome;	
+	private String prazoCorrente;
+	private String prazoIntermediaria;	
+	private String destinacaoFinal;
+	
+	public ClasseDto(Classe classe) {
+		this.codigo = classe.getCodigo();
+		this.nome = classe.getNome();
+		this.prazoCorrente = classe.getPrazoCorrente();
+		this.destinacaoFinal = classe.getDestinacaoFinal().toString().toLowerCase();
+	}
+}
