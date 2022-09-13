44a86445455248e77d9b5fe6c1bcfd245ab38b62
hgrafa <hugorafell@gmail.com>
2022-09-13 15:27:28 -0300

    Updating ClasseForm: Adding toClasse method

--- src/main/java/br/com/sigad/controllers/form/ClasseForm.java
+++ src/main/java/br/com/sigad/controllers/form/ClasseForm.java
@@ -1,27 +1,48 @@
 package br.com.sigad.controllers.form;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
 
-import br.com.sigad.entities.enums.Destinacao;
-import br.com.sigad.entities.enums.GrauSigilo;
-import br.com.sigad.entities.enums.Permissao;
+import br.com.sigad.entities.Classe;
 import br.com.sigad.entities.enums.Sigilo;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter @NoArgsConstructor
 public class ClasseForm {
-
+	
+	@NotBlank
 	private Long codigo;
+	
+	@NotBlank
 	private String nome;	
+	
 	private String prazoCorrente;
 	private String prazoIntermediaria;	
 	private String observacao;
 	
+	// boolean
 	private String indicadorAtiva;	
 	
-	private String permissaoDeUso;
-	private String destinacaoFinal;
+	// enums
 	private String sigilo;
 	private String grauSigilo;
+	private String destinacaoFinal;
+	private String permissaoDeUso;
 	
+	public Classe toClasse() {
+		Classe classeEntity = new Classe();
+	
+		classeEntity.setIndicadorAtiva(indicadorAtiva.equalsIgnoreCase("ativo"));
+		
+		classeEntity.setCodigo(codigo);
+		classeEntity.setNome(nome);
+		// ...
+		
+		// modelo de enum
+		classeEntity.setSigilo(Sigilo.valueOf(sigilo.toUpperCase()));
+		
+		return classeEntity;
+	}
 	
 }
