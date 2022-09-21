6f853939d78f44c024c73990e60aa7b57f6a128a
hgrafa <hugorafell@gmail.com>
2022-09-21 07:21:32 -0300

    Adding methods to ClasseService

--- src/main/java/br/com/sigad/services/ClasseService.java
+++ src/main/java/br/com/sigad/services/ClasseService.java
@@ -1,5 +1,6 @@
 package br.com.sigad.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -8,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.sigad.entities.Classe;
+import br.com.sigad.entities.SubClasse;
+import br.com.sigad.entities.enums.Destinacao;
+import br.com.sigad.entities.enums.GrauSigilo;
+import br.com.sigad.entities.enums.Permissao;
 import br.com.sigad.entities.enums.Sigilo;
 import br.com.sigad.repositories.ClasseRepository;
 
@@ -17,6 +22,18 @@ public class ClasseService {
 	@Autowired
 	private ClasseRepository classeRepository;
 	
+	public void save() {
+		List<Classe> input = new ArrayList<>();
+
+		input.add(new Classe(2L, "b1", "c", "d", "e", true, Permissao.ESTRUTURA_HIERARQUICA, Destinacao.ELIMINACAO, Sigilo.OSTENSIVO, GrauSigilo.CONFIDENCIAL));
+
+		input.add(new Classe(2L, "b2", "c", "d", "e", true, Permissao.ESTRUTURA_HIERARQUICA, Destinacao.ELIMINACAO, Sigilo.OSTENSIVO, GrauSigilo.CONFIDENCIAL));
+
+		input.add(new Classe( 2L, "b3", "c", "d", "e", true, Permissao.ESTRUTURA_HIERARQUICA, Destinacao.ELIMINACAO, Sigilo.OSTENSIVO, GrauSigilo.CONFIDENCIAL));
+
+		classeRepository.saveAll(input);
+	}
+
 	public List<Classe> findAll() {
 		List<Classe> classes = classeRepository.findAll();
 		return classes;
