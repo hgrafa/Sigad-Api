7ce07387836dea65b71f4257d4c0accb707ea83d
hgrafa <hugorafell@gmail.com>
2022-09-21 07:21:47 -0300

    Adding more endpoints in ClasseController

--- src/main/java/br/com/sigad/resources/ClasseController.java
+++ src/main/java/br/com/sigad/resources/ClasseController.java
@@ -1,13 +1,20 @@
 package br.com.sigad.resources;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+import javax.websocket.server.PathParam;
+
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.sigad.controllers.dto.ClasseDto;
 import br.com.sigad.entities.Classe;
 import br.com.sigad.entities.enums.Destinacao;
 import br.com.sigad.entities.enums.GrauSigilo;
@@ -16,18 +23,29 @@ import br.com.sigad.entities.enums.Sigilo;
 import br.com.sigad.services.ClasseService;
 
 @RestController
-@RequestMapping(value = "/classes")
+@RequestMapping(value = "api/0.0.1/classes")
 public class ClasseController {
 
 	@Autowired
 	private ClasseService classeService;
 		
 	@GetMapping
-	public ResponseEntity<List<Classe>> findAll() {
-		List<Classe> classes = classeService.findAll();
+	public ResponseEntity<List<ClasseDto>> findAll() {
+		classeService.save();
+		List<ClasseDto> classes = classeService
+																.findAll()
+																.stream()
+																.map(c -> new ClasseDto(c))
+																.collect(Collectors.toList());
+
 		return ResponseEntity.ok().body(classes);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClasseDto> findById(@PathVariable Long id){
+		ClasseDto classe  = new ClasseDto( classeService.findById(id) );
+		return ResponseEntity.ok().body(classe);
+	}
 	
 	
 }
