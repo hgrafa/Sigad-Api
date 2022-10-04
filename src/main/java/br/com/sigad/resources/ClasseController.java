287d29eae1b15a91c1b8d2b4bad38d42449d979a
hgrafa <hugorafell@gmail.com>
2022-10-04 09:03:44 -0300

    Updating class controller to Find By id

--- src/main/java/br/com/sigad/resources/ClasseController.java
+++ src/main/java/br/com/sigad/resources/ClasseController.java
@@ -1,25 +1,20 @@
 package br.com.sigad.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-import javax.websocket.server.PathParam;
-
-import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sigad.controllers.dto.ClasseDto;
-import br.com.sigad.entities.Classe;
-import br.com.sigad.entities.enums.Destinacao;
-import br.com.sigad.entities.enums.GrauSigilo;
-import br.com.sigad.entities.enums.Permissao;
-import br.com.sigad.entities.enums.Sigilo;
+import br.com.sigad.controllers.form.ClasseForm;
 import br.com.sigad.services.ClasseService;
 
 @RestController
@@ -31,8 +26,10 @@ public class ClasseController {
 
 	@GetMapping
 	public ResponseEntity<List<ClasseDto>> findAll() {
-		classeService.save();
-		List<ClasseDto> classes = classeService.findAll().stream().map(c -> new ClasseDto(c))
+		List<ClasseDto> classes = classeService
+				.findAll()
+				.stream()
+				.map(c -> new ClasseDto(c))
 				.collect(Collectors.toList());
 
 		return ResponseEntity.ok().body(classes);
