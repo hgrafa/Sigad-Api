4e6e3889df952895332bc5aaea3f61cbe8f45922
hgrafa <hugorafell@gmail.com>
2022-10-11 20:11:42 -0300

    Adding Http status code in exceptions

--- src/main/java/br/com/sigad/services/exceptions/ClasseNaoEncontradaException.java
+++ src/main/java/br/com/sigad/services/exceptions/ClasseNaoEncontradaException.java
@@ -1,10 +1,16 @@
 package br.com.sigad.services.exceptions;
 
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
 public class ClasseNaoEncontradaException extends RuntimeException {
+
+  private HttpStatus status = HttpStatus.NOT_FOUND;
 
   public ClasseNaoEncontradaException(String message) {
     super(message);
   }
 
-  
 }
