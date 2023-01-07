54e068662f78f8d6701f72addf399f055deef4ff
hgrafa <hugorafell@gmail.com>
2023-01-06 23:59:10 -0300

    adding new repository methods

--- src/main/java/br/com/sigad/repositories/ClasseRepository.java
+++ src/main/java/br/com/sigad/repositories/ClasseRepository.java
@@ -1,11 +1,18 @@
 package br.com.sigad.repositories;
 
+import br.com.sigad.entities.enums.Destinacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.sigad.entities.Classe;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ClasseRepository extends JpaRepository<Classe, Long>{
+    Optional<Classe> findByNome(String nome);
+    List<Classe> findAllByDestinacao(Destinacao destinacao);
 
 }
