726ff6a6257c84d2eae06bc56d505db9b3345c18
hgrafa <hugorafell@gmail.com>
2023-02-05 20:16:24 -0300

    Adding Login Web Controller

---
+++ src/main/java/br/com/sigad/controllers/users/LoginController.java
@@ -0,0 +1,11 @@
+package br.com.sigad.controllers.users;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+
+}
