0776738d723623ab4a4c27593d4b085ca7f2e50d
Gillian Queiroga <gillianqueiroga@gmail.com>
2023-03-18 16:46:59 -0300

    Updating ClasseDto.java Changes by hugor

--- src/main/java/br/com/sigad/model/dto/input/ClasseForm.java
+++ src/main/java/br/com/sigad/model/dto/input/ClasseForm.java
@@ -2,6 +2,7 @@ package br.com.sigad.model.dto.input;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,21 +13,29 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ClasseForm {
 
-	// @NotBlank(message = "O código não deve estar em branco.")
 	private Long codigo;
-	@NotBlank(message = "O código nome deve estar em branco.")
+
+	@NotBlank(message = "O nome deve estar em branco.")
 	private String nome;
+
 	private String prazoCorrente;
 	private String prazoIntermediaria;
 	private String observacao;
 
 	// goes to boolean
+	@NotBlank(message = "O indicador ativa deve estar em branco.")
 	private String indicadorAtiva;
 
 	// goes to enums
+	// TODO
+	@NotBlank(message = "O código nome deve estar em branco.")
 	private String permissaoDeUso;
+
 	private String destinacaoFinal;
+
+	@NotBlank(message = "O código nome deve estar em branco.")
 	private String sigilo;
+
 	private String grauSigilo;
 
 }
