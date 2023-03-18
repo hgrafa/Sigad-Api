a5ca710e93d664a2c1c171e84bd9fad827ecb06a
Gillian Queiroga <gillianqueiroga@gmail.com>
2023-03-18 16:51:49 -0300

    Updating GrauSigilo.java Changes by hugor

--- src/main/java/br/com/sigad/model/enums/GrauSigilo.java
+++ src/main/java/br/com/sigad/model/enums/GrauSigilo.java
@@ -1,8 +1,23 @@
 package br.com.sigad.model.enums;
 
-public enum GrauSigilo {
-	CONFIDENCIAL,
-	RESERVADO,
-	SECRETO,
-	ULTRA_SECRETO;
+import br.com.sigad.model.interfaces.EnumWithText;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter @AllArgsConstructor
+public enum GrauSigilo implements EnumWithText {
+	// TODO adicionar getter + AllArgs + atributo text
+
+	CONFIDENCIAL("Confidencial"),
+	RESERVADO("Reservado"),
+	SECRETO("Secreto"),
+	ULTRA_SECRETO("Ultra Secreto");
+
+	private String absoluteText;
+	private String parsedText;
+
+	GrauSigilo(String text) {
+		this.parsedText = text;
+		this.absoluteText = text.replaceAll(" ", "_").toUpperCase();
+	}
 }
