b02bb19d72930bc3575f0964cee8fc31d1b725f0
Gillian Queiroga <gillianqueiroga@gmail.com>
2023-03-18 16:49:51 -0300

    Updating Destinacao.java Changes by hugor

--- src/main/java/br/com/sigad/model/enums/Destinacao.java
+++ src/main/java/br/com/sigad/model/enums/Destinacao.java
@@ -1,8 +1,22 @@
 package br.com.sigad.model.enums;
 
-public enum Destinacao {
-	RECOLHIMENTO,
-	ELIMINACAO;
+import br.com.sigad.model.interfaces.EnumWithText;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter @AllArgsConstructor
+public enum Destinacao implements EnumWithText {
+	// TODO adicionar getter + AllArgs + atributo text
+
+	RECOLHIMENTO("Recolhimento"),
+	ELIMINACAO("Eliminação");
+
+	private String absoluteText;
+	private String parsedText;
+
+	Destinacao(String text) {
+		this.parsedText = text;
+		this.absoluteText = text.replaceAll(" ", "_").toUpperCase();
+	}
 
 }
