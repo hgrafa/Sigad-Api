6b3cf90fb17b403814b19402102f19aaf84f5beb
Gillian Queiroga <gillianqueiroga@gmail.com>
2023-03-18 16:50:28 -0300

    Updating DropdownOption.java Changes by hugor

--- src/main/java/br/com/sigad/model/util/DropdownOption.java
+++ src/main/java/br/com/sigad/model/util/DropdownOption.java
@@ -1,12 +1,13 @@
 package br.com.sigad.model.util;
 
+import br.com.sigad.model.interfaces.EnumWithText;
+
 public record DropdownOption(
         String value,
         String text
 ) {
 
-    public DropdownOption(String sharedValue) {
-        this(sharedValue, sharedValue);
+    public <E extends Enum<E> & EnumWithText> DropdownOption(E myEnum) {
+        this(myEnum.getAbsoluteText(),  myEnum.getParsedText());
     }
-
 }
