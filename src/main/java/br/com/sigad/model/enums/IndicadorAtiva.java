252c0303f69c50e887b46d34252735e83e71c20f
Gillian Queiroga <gillianqueiroga@gmail.com>
2023-03-19 18:57:10 -0300

    Updating IndicadorAtiva.java

--- src/main/java/br/com/sigad/model/enums/IndicadorAtiva.java
+++ src/main/java/br/com/sigad/model/enums/IndicadorAtiva.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 
 @Getter @AllArgsConstructor
 public enum IndicadorAtiva implements EnumWithText {
-    // TODO adicionar getter + AllArgs + atributo text
 
     ATIVO("Ativo"),
     INATIVO("Inativo");
